38c0b87cc8b3029d77e8d1109d33bd4844fd5209
Bernardo Escudeiro <73354104+jbtescudeiro16@users.noreply.github.com>
2022-11-25 18:15:39 +0000

    Update Client.java

--- src/Cliente/Client.java
+++ src/Cliente/Client.java
@@ -7,11 +7,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-/**
- * This class implements java socket client
- * @author pankaj
- *
- */
+
 public class Client {
 
     public static void main(String[] args) throws IOException {
