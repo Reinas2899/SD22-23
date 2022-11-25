81cc2eca06c891f4152252f300ad495fd466a63c
Bernardo Escudeiro <73354104+jbtescudeiro16@users.noreply.github.com>
2022-11-25 18:13:58 +0000

    Add files via upload

--- src/Cliente/Client.java
+++ src/Cliente/Client.java
@@ -1,7 +1,28 @@
+package TP;
+
+
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * This class implements java socket client
+ * @author pankaj
+ *
+ */
 public class Client {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+    Socket s = new Socket("localhost",4999);
+    PrintWriter pr = new PrintWriter(s.getOutputStream());
+    pr.println("hello");
+    pr.flush();
+    InputStreamReader in = new InputStreamReader(s.getInputStream());
+    BufferedReader bf= new BufferedReader(in);
 
-        System.out.println("Inicio");
+    String str = bf.readLine();
+        System.out.println("Server:"+str);
     }
 }
